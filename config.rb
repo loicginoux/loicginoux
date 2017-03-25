559b6786147a995d823baadf84cc93d6615b632d
Loic Ginoux <lginoux@gmail.com>
2017-03-25 19:11:10 +0100

    change mention legales layout

--- config.rb
+++ config.rb
@@ -41,7 +41,6 @@ page '/*.txt', layout: false
 
 # With alternative layout
 # page "/index.html", layout: :index
-# page "/mentions_legales.html", layout: :index
 
 # Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
