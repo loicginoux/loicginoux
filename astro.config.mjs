360755994e11a0812106ba74bbd892f2fec8431b
Loic Ginoux <lginoux@gmail.com>
2024-01-15 14:33:16 +0100

    french version

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,15 @@ export default defineConfig({
   site: 'https://loicginoux.com',
   integrations: [tailwind(), sitemap()],
   output: "hybrid",
-  adapter: netlify()
+  adapter: netlify(),
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "fr"],
+    routing: {
+      prefixDefaultLocale: false
+    },
+    fallback: {
+      fr: "en"
+    }
+  }
 });
